98c1bac32558c2803a6f33ed77f31ba2db26182c
John Ovalle <johnovalle@gmail.com>
2017-08-07 17:31:45 +0000

    bootstrap app

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-console.log('loaded_hot');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
